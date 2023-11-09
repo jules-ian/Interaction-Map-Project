c03679ccfcba104340986924432d339f4533abcc
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-09 22:12:14 +0100

    Add professional entity

---
+++ backend/InteractiveMapProject/InteractiveMapProject.Contracts/Entities/Professional.cs
@@ -0,0 +1,32 @@
+using InteractiveMapProject.Contracts.Enums;
+
+namespace InteractiveMapProject.Contracts.Entities;
+
+public class Professional
+{
+    public string ServiceName { get; set; } = default!;
+
+    public string Address { get; set; } = default!;
+
+    public int PhoneNumber { get; set; }
+
+    public string Email { get; set; } = default!;
+
+    public string ResourcePersonName { get; set; } = default!;
+
+    public string Function { get; set; } = default!;
+
+    public int ContactPersonPhoneNumber { get; set; }
+
+    public string ContactPersonEmail { get; set; } = default!;
+
+    public FieldOfIntervention FieldOfIntervention { get; set; } = default!;
+
+    public Mission Mission { get; set; } = default!;
+
+    public Geolocation Geolocation { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
+
+    public DateTime CreationDateTime { get; set; }
+}
