be6971bb7ef7aaa0c3441e78314447abb7902743
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-09 22:45:44 +0100

    Apply first migration

---
+++ backend/InteractiveMapProject/InteractiveMapProject.Data.Db/Migrations/20231109214237_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InteractiveMapProject.Data.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Professionals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address_PostalCode = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ResourcePersonName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Function = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactPersonPhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    ContactPersonEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FieldOfIntervention_Audience = table.Column<int>(type: "int", nullable: false),
+                    FieldOfIntervention_SectorOfIntervetion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FieldOfIntervention_PlaceOfIntervention = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mission = table.Column<int>(type: "int", nullable: false),
+                    Geolocation_Latitude = table.Column<double>(type: "float", nullable: false),
+                    Geolocation_Longitude = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professionals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Professionals");
+        }
+    }
+}
