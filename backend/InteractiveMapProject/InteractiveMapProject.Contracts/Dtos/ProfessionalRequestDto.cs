333edb459cef47051b4de5e1e1f1b118a2203245
AnaGrskovic <ana.grskovic56@gmail.com>
2023-11-12 19:19:37 +0100

    Add dtos

--- backend/InteractiveMapProject/InteractiveMapProject.Contracts/Dtos/ProfessionalRequestDto.cs
+++ backend/InteractiveMapProject/InteractiveMapProject.Contracts/Dtos/ProfessionalRequestDto.cs
@@ -1,5 +1,29 @@
+using InteractiveMapProject.Contracts.Entities;
+using InteractiveMapProject.Contracts.Enums;
+
 namespace InteractiveMapProject.Contracts.Dtos;
 
 public class ProfessionalRequestDto
 {
+    public string ServiceName { get; set; } = default!;
+
+    public Address Address { get; set; } = default!;
+
+    public int PhoneNumber { get; set; }
+
+    public string Email { get; set; } = default!;
+
+    public string ResourcePersonName { get; set; } = default!;
+
+    public string Function { get; set; } = default!;
+
+    public int ContactPersonPhoneNumber { get; set; }
+
+    public string ContactPersonEmail { get; set; } = default!;
+
+    public FieldOfIntervention FieldOfIntervention { get; set; } = default!;
+
+    public Mission Mission { get; set; } = default!;
+
+    public string Description { get; set; } = default!;
 }
