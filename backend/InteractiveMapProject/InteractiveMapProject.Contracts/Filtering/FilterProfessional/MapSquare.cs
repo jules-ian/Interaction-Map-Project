a104c61bbaa6257e424e620b1bd1a8b19609b289
AnaGrskovic <ana.grskovic56@gmail.com>
2023-12-19 14:45:14 +0100

    Fix coordinate validation

--- backend/InteractiveMapProject/InteractiveMapProject.Contracts/Filtering/FilterProfessional/MapSquare.cs
+++ backend/InteractiveMapProject/InteractiveMapProject.Contracts/Filtering/FilterProfessional/MapSquare.cs
@@ -1,11 +1,9 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace InteractiveMapProject.Contracts.Filtering.FilterProfessional;
 
 public class MapSquare
 {
-    public double NorthWestLatitude { get; set; } = default!;
-    public double NorthWestLongitude { get; set; } = default!;
-    public double SouthEastLatitude { get; set; } = default!;
-    public double SouthEastLongitude { get; set; } = default!;
+    public double? NorthWestLatitude { get; set; }
+    public double? NorthWestLongitude { get; set; }
+    public double? SouthEastLatitude { get; set; }
+    public double? SouthEastLongitude { get; set; }
 }
