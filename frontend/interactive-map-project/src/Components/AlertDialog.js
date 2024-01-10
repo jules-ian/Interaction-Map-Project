548a9ae98027f39f0a544d2145607c589eeecd8d
Jonas Werner <jonas.werner-1@studium.uni-hamburg.de>
2024-01-10 16:09:28 +0100

    fixed strings in fr and en

--- frontend/interactive-map-project/src/Components/AlertDialog.js
+++ frontend/interactive-map-project/src/Components/AlertDialog.js
@@ -1,11 +1,13 @@
 // AlertDialog.js
 import React from "react";
 import { Dialog, DialogContent, DialogTitle, Button } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 export const SuccessDialog = ({ open, onClose, message }) => {
+  const { t } = useTranslation();
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Success</DialogTitle>
+      <DialogTitle>{t("common.success")}</DialogTitle>
       <DialogContent>{message}</DialogContent>
       <Button onClick={onClose} color="primary" autoFocus>
         Close
@@ -15,9 +17,11 @@ export const SuccessDialog = ({ open, onClose, message }) => {
 };
 
 export const ErrorDialog = ({ open, onClose, message }) => {
+  const { t } = useTranslation();
+
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Error</DialogTitle>
+      <DialogTitle>{t("common.error")}</DialogTitle>
       <DialogContent>{message}</DialogContent>
       <Button onClick={onClose} color="primary" autoFocus>
         Close
