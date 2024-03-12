867fd9ad2d6e2b2f6e3b0a2dbaa54c6cc318aa04
PDLAProject <le.norgeux@gmail.com>
2024-03-12 15:15:44 +0100

    correct spelling + comment

--- frontend/interactive-map-project/src/utils/ArrayFunctions.js
+++ frontend/interactive-map-project/src/utils/ArrayFunctions.js
@@ -4,7 +4,7 @@ export const arrayRange = (start, stop, step) =>
     (value, index) => start + index * step
   );
 
-export const mapNamesToIDs = function (nameArray, tupleArray) {
+export const mapNamesToIDs = function (nameArray, tupleArray) { // ? chaque professionnel associé à un id ?
   let ids = nameArray.map((name) => {
     const match = tupleArray.find((tuple) => tuple.name === name);
     return match ? match.id : null;
