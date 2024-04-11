385d1cb3a1a2ff3f8f1e8106809f74bdf3c12a6c
jules-ian <julesian.barnavon@gmail.com>
2024-04-11 19:46:37 +0200

    Added Roles User Professional and Admin

--- backend/InteractiveMapProject/InteractiveMapProject.API/Program.cs
+++ backend/InteractiveMapProject/InteractiveMapProject.API/Program.cs
@@ -106,14 +106,14 @@ public class Program
         {
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
             var roles = new[] { "User", "Professional", "Admin" };
-/*
+
             foreach (var role in roles)
             {
                 if (!await roleManager.RoleExistsAsync(role))
                 {
                     await roleManager.CreateAsync(new IdentityRole(role));
                 }
-            }*/
+            }
 
         }
 
